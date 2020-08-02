f759b889f97b3c2fc48452da63e8718b2ce10115
vakhtang <hazardland@gmail.com>
2020-08-02 21:34:31 +0400

    setting cmd title, removed 1st symbol in powerline

--- _set.bat
+++ _set.bat
@@ -1,11 +1,13 @@
 @echo off
 
+for %%a in ("%CD%") do set "PARENT_FOLDER=%%~nxa"
+title %PARENT_FOLDER%
+
 set GITBRANCH=
 for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
 
 if "%GITBRANCH%" == "" (
-    prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
+    prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
 ) else (
-    prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+    prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
 )
-
