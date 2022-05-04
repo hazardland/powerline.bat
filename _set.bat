e073af43a2d07326afc51bd1563ef01bde606960
XU Zhaofeng <zhaofeng.xu@iag.co.nz>
2022-05-04 22:04:55 +1200

    Change the font's foreground colour to white for the folder path. The original black foreground with the blue background is a bit hard to read.

--- _set.bat
+++ _set.bat
@@ -4,7 +4,7 @@
         set GITBRANCH=
         for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
         if "%GITBRANCH%" == "" (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
+            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34m$E[0m$S
         ) else (
-            prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
         )
