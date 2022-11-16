2c8c26d48d7e531ec563cd21f24d4e8721f5859e
APerricone <antonino.perricone@yahoo.it>
2022-11-16 15:28:19 +0100

    restored initial arrow

--- _set.bat
+++ _set.bat
@@ -11,15 +11,15 @@
         chcp 65001
         if "%ELEVATED%" == "" (
             if "%GITBRANCH%" == "" (
-                prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
+                prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34m$E[0m$S
             ) else (
-                prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+                prompt $E[30;44m$S$E[0m$E[30;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
             )
         ) else (
             if "%GITBRANCH%" == "" (
-                prompt $E[30;41m$S$E[0m$E[30;41m$P$S$E[0m$E[31m$E[0m$S
+                prompt $E[30;41m$S$E[0m$E[30;41m$P$S$E[0m$E[31m$E[0m$S
             ) else (
-                prompt $E[30;41m$S$E[0m$E[30;41m$P$S$E[0m$E[31;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+                prompt $E[30;41m$S$E[0m$E[30;41m$P$S$E[0m$E[31;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
             )
         )
         chcp 850
