592dac80eac8118c4bb7bcb0dde7df7e4662b3c0
APerricone <antonino.perricone@yahoo.it>
2022-11-22 15:06:49 +0100

    added svn segment

--- _set.bat
+++ _set.bat
@@ -4,6 +4,8 @@
 
         set GITBRANCH=
         for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
+        set SVNREV=
+        for /f %%I in ('svn info --show-item revision 2^> nul') do set SVNREV=%%I
 
         set ELEVATED=
         net.exe session 1>NUL 2>NUL && set ELEVATED=1
@@ -11,8 +13,9 @@
         chcp 65001 1>NUL 2>NUL
         if "%ELEVATED%" == "" (set CLR=4) else (set CLR=1)
         if "%GITBRANCH%" == "" (SET GIT=) else (set GIT=;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33)
+        if "%SVNREV%" == "" (SET SVN=) else (set SVN=;46m$S$E[0m$E[30;46mr.%SVNREV%$S$E[0m$E[36)
 
-        prompt $E[30;4%CLR%m$S$E[0m$E[30;4%CLR%m$P$S$E[0m$E[3%CLR%%GIT%m$E[0m$S
+        prompt $E[30;4%CLR%m$S$E[0m$E[30;4%CLR%m$P$S$E[0m$E[3%CLR%%GIT%%SVN%m$E[0m$S
 
         chcp 850 1>NUL 2>NUL
 
